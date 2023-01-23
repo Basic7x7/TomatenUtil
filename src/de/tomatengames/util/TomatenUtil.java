50f317f580e5bfdc5c8af49e2d3110644a2dcb12
Basic7x7 <basic7x7@gmail.com>
2023-01-23 20:52:16 +0100

    #5 Added the getVersion() method

---
+++ src/de/tomatengames/util/TomatenUtil.java
@@ -0,0 +1,23 @@
+package de.tomatengames.util;
+
+/**
+ * The main class of the TomatenUtil library.
+ * 
+ * @author Basic7x7
+ * @version 2023-01-23
+ * @since 1.0
+ */
+public class TomatenUtil {
+	
+	// Static class.
+	private TomatenUtil() {
+	}
+	
+	/**
+	 * Returns the version of the installed TomatenUtil library.
+	 * @return The version.
+	 */
+	public static String getVersion() {
+		return "1.0";
+	}
+}
