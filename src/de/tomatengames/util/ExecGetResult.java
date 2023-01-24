a6ad5cd2604116990dc3aed945d94d8a624bba78
Basic7x7 <basic7x7@gmail.com>
2023-01-24 22:59:37 +0100

    #5 Documented the ExecGetResult class

--- src/de/tomatengames/util/ExecGetResult.java
+++ src/de/tomatengames/util/ExecGetResult.java
@@ -1,5 +1,12 @@
 package de.tomatengames.util;
 
+/**
+ * The result of a <i>get</i> process.
+ * 
+ * @author Basic7x7
+ * @version 2023-01-23
+ * @since 1.0
+ */
 public class ExecGetResult {
 	private final CharSequence output;
 	private final CharSequence error;
@@ -11,10 +18,24 @@ public class ExecGetResult {
 		this.exitValue = exitValue;
 	}
 	
+	/**
+	 * Returns the standard output of the process.
+	 * <p>
+	 * Note: The returned value might not be a {@link String}.
+	 * Use {@link CharSequence#toString()}.
+	 * @return The standard output.
+	 */
 	public CharSequence getOutput() {
 		return this.output;
 	}
 	
+	/**
+	 * Returns the error output of the process.
+	 * <p>
+	 * Note: The returned value might not be a {@link String}.
+	 * Use {@link CharSequence#toString()}.
+	 * @return The error output.
+	 */
 	public CharSequence getError() {
 		return this.error;
 	}
