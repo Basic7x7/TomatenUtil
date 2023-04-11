41412e70642bf130de886a623137937fee48c0b2
Basic7x7 <basic7x7@gmail.com>
2023-04-11 20:29:07 +0200

    #18 Add CharsetUtil.encodeUTF8(codePoint, OutputStream)

--- src/de/tomatengames/util/CharsetUtil.java
+++ src/de/tomatengames/util/CharsetUtil.java
@@ -1,5 +1,8 @@
 package de.tomatengames.util;
 
+import java.io.IOException;
+import java.io.OutputStream;
+
 /**
  * Provides utilities to work with character encodings.
  * 
@@ -59,6 +62,45 @@ public class CharsetUtil {
 					| ((codePoint << 4) & 0b00000_000__00_111111__00_000000__00_000000)
 					// Bits 18-21 -> 24-27
 					| ((codePoint << 6) & 0b00000_111__00_000000__00_000000__00_000000);
+		}
+		throw new IllegalArgumentException("Invalid code point " + Integer.toHexString(codePoint) + "!");
+	}
+	
+	
+	/**
+	 * Encodes the specified Unicode code point using UTF-8
+	 * and writes the result into the {@link OutputStream}.
+	 * @param codePoint The code point that should be encoded.
+	 * @param out The output stream.
+	 * @throws IOException If an I/O error occurs.
+	 * @throws IllegalArgumentException If the code point is out of range.
+	 */
+	public static void encodeUTF8(int codePoint, OutputStream out) throws IOException {
+		if (codePoint < 0) {
+			throw new IllegalArgumentException("Invalid code point " + Integer.toHexString(codePoint) + "!");
+		}
+		if (codePoint <= 0x7F) {
+			out.write(codePoint);
+			return;
+		}
+		if (codePoint <= 0x7FF) {
+			out.write(0b110_00000 | ((codePoint >>> 6) & 0b11111)); // Bits 6-11
+			out.write(0b10_000000 | (codePoint & 0b111111)); // Bits 0-6
+			return;
+		}
+		if (codePoint <= 0xFFFF) {
+			out.write(0b1110_0000 | ((codePoint >>> 12) & 0b1111)); // Bits 12-16
+			out.write(0b10_000000 | ((codePoint >>> 6) & 0b111111)); // Bits 6-12
+			out.write(0b10_000000 | (codePoint & 0b111111)); // Bits 0-6
+			return;
+		}
+		// Only allow 0x10FFFF codePoints [https://www.rfc-editor.org/rfc/rfc3629#section-3]
+		if (codePoint <= 0x10FFFF) {
+			out.write(0b11110_000 | ((codePoint >>> 18) & 0b111)); // Bits 18-21
+			out.write(0b10_000000 | ((codePoint >>> 12) & 0b111111)); // Bits 12-18
+			out.write(0b10_000000 | ((codePoint >>> 6) & 0b111111)); // Bits 6-12
+			out.write(0b10_000000 | (codePoint & 0b111111)); // Bits 0-6
+			return;
 		}
 		throw new IllegalArgumentException("Invalid code point " + Integer.toHexString(codePoint) + "!");
 	}
