634045bc7857233114967fe6893bd45c3075d87f
Basic7x7 <basic7x7@gmail.com>
2024-11-18 10:09:39 +0000

    Update the version number to 1.7

--- src/de/tomatengames/util/TomatenUtil.java
+++ src/de/tomatengames/util/TomatenUtil.java
@@ -4,7 +4,8 @@ package de.tomatengames.util;
  * The main class of the TomatenUtil library.
  * 
  * @author Basic7x7
- * @version 2023-01-23
+ * @version 2024-11-18 last modified
+ * @version 2023-01-23 created
  * @since 1.0
  */
 public class TomatenUtil {
@@ -18,6 +19,6 @@ public class TomatenUtil {
 	 * @return The version.
 	 */
 	public static String getVersion() {
-		return "1.6";
+		return "1.7";
 	}
 }
