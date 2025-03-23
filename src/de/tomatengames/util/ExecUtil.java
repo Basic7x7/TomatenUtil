cb663b2701adc66b832ca4020146c0cb16530000
LukasE7x7 <i@c4.tf>
2025-03-23 21:18:54 +0100

    #55 Support input parameter and bytes in ExecUtil

--- src/de/tomatengames/util/ExecUtil.java
+++ src/de/tomatengames/util/ExecUtil.java
@@ -1,10 +1,9 @@
 package de.tomatengames.util;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
+import java.io.OutputStream;
 import java.util.concurrent.atomic.AtomicReference;
 
 /**
@@ -28,39 +27,45 @@ public class ExecUtil {
 	 * @param envp Environment variables in the format specified by {@link Runtime#exec(String[], String[], File)}.
 	 * May be {@code null}.
 	 * @param dir The working directory of the subprocess. May be {@code null}.
-	 * @return An result object containing the standard output of the process, the exit value, ...
+	 * @param input Bytes to write to the standard input of the subprocess. May be {@code null}.
+	 * @return An result object containing the standard output of the subprocess, the exit value, ...
 	 * @throws IOException If an error occurs.
 	 */
-	public static ExecGetResult execGet(String[] cmd, String[] envp, File dir) throws IOException {
+	public static ExecGetResult execGet(String[] cmd, String[] envp, File dir, byte[] input) throws IOException {
 		Process process = Runtime.getRuntime().exec(cmd, envp, dir);
 		
-		StringBuilder output = new StringBuilder();
-		AtomicReference<IOException> outputException = new AtomicReference<>(null);
-		Thread outputThread = readAsync(process.getInputStream(), output, outputException);
+		AtomicReference<IOException> ioException = new AtomicReference<>(null);
 		
-		StringBuilder error = new StringBuilder();
-		AtomicReference<IOException> errorException = new AtomicReference<>(null);
-		Thread errorThread = readAsync(process.getErrorStream(), error, errorException);
+		Thread inputThread = input == null ? null : asyncIO(() -> {
+			OutputStream out = process.getOutputStream();
+			out.write(input);
+			out.flush();
+			out.close();
+		}, ioException);
+		
+		ByteArrayOutputStream output = new ByteArrayOutputStream();
+		Thread outputThread = asyncIO(() -> IOUtil.pipeStream(process.getInputStream(), output), ioException);
+		
+		ByteArrayOutputStream error = new ByteArrayOutputStream();
+		Thread errorThread = asyncIO(() -> IOUtil.pipeStream(process.getErrorStream(), error), ioException);
 		
 		try {
 			process.waitFor();
+			if (inputThread != null)
+				inputThread.join();
 			outputThread.join();
 			errorThread.join();
 		} catch (InterruptedException e) {
 			throw new IOException(e);
 		}
 		
-		IOException outEx = outputException.get();
-		if (outEx != null) {
-			throw outEx;
-		}
-		IOException errEx = errorException.get();
-		if (errEx != null) {
-			throw errEx;
+		IOException ex = ioException.get();
+		if (ex != null) {
+			throw ex;
 		}
 		
 		int exitValue = process.exitValue();
-		return new ExecGetResult(output, error, exitValue);
+		return new ExecGetResult(output.toByteArray(), error.toByteArray(), exitValue);
 	}
 	
 	/**
@@ -72,7 +77,7 @@ public class ExecUtil {
 	 * @throws IOException If an error occurs.
 	 */
 	public static ExecGetResult execGet(String... cmd) throws IOException {
-		return execGet(cmd, null, null);
+		return execGet(cmd, null, null, null);
 	}
 	
 	/**
@@ -89,12 +94,12 @@ public class ExecUtil {
 	 * @see #execGet(String[], String[], File)
 	 */
 	public static String execGetOutput(String[] cmd, String[] envp, File dir) throws IOException {
-		ExecGetResult result = execGet(cmd, envp, dir);
+		ExecGetResult result = execGet(cmd, envp, dir, null);
 		if (result.getExitValue() != 0) {
 			throw new IOException("Command '" + String.join(" ", cmd) + "' returned exit value "
-					+ result.getExitValue() + ". " + "Error stream: " + result.getError());
+					+ result.getExitValue() + ". " + "Error stream: " + result.getErrorString());
 		}
-		return result.getOutput().toString();
+		return result.getOutputString();
 	}
 	
 	/**
@@ -111,20 +116,25 @@ public class ExecUtil {
 		return execGetOutput(cmd, null, null);
 	}
 	
-	private static Thread readAsync(InputStream in, StringBuilder result, AtomicReference<IOException> exception) {
+	private static Thread asyncIO(IORunnable run, AtomicReference<IOException> exception) {
 		Thread thread = new Thread(() -> {
-			try (Reader reader = new InputStreamReader(in)) {
-				int ch;
-				while ((ch = reader.read()) >= 0) {
-					result.append((char) ch);
-				}
+			try {
+				run.run();
 			} catch (IOException e) {
-				exception.set(e);
+				// set exception if it is the first
+				if (!exception.compareAndSet(null, e)) {
+					// otherwise add to suppressed of first
+					exception.get().addSuppressed(e);
+				}
 			}
 		});
 		thread.setDaemon(true);
 		thread.start();
 		return thread;
+	}
+	
+	private static interface IORunnable {
+		void run() throws IOException;
 	}
 	
 }
