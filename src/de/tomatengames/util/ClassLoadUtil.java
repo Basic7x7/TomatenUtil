e70f730d372db0b55129d3538fd050e450298c65
LukasE7x7 <i@c4.tf>
2025-04-08 21:26:19 +0200

    #58 Document ClassLoadUtil

--- src/de/tomatengames/util/ClassLoadUtil.java
+++ src/de/tomatengames/util/ClassLoadUtil.java
@@ -7,11 +7,25 @@ import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
+/**
+ * Provides utility methods related to class loading.
+ * Currently focused on methods to load all classes from given jar files.
+ * 
+ * @version 2025-04-08 created
+ * @since 1.8
+ */
 public class ClassLoadUtil {
 	
 	private ClassLoadUtil() {
 	}
 	
+	/**
+	 * Retrieves all fully qualified class names from the specified JAR file by
+	 * scanning it for entries whose name ends with ".class".
+	 * 
+	 * @param jar the ZIP/JAR file to find classes in
+	 * @return a collection of fully qualified class names
+	 */
 	public static Collection<String> getAllClassNames(ZipFile jar) {
 		List<String> classNames = new ArrayList<>();
 		Enumeration<? extends ZipEntry> it = jar.entries();
@@ -25,6 +39,16 @@ public class ClassLoadUtil {
 		return classNames;
 	}
 	
+	/**
+	 * Loads all classes specified by the given class names using the provided class
+	 * loader.
+	 * 
+	 * @param classNames  a collection of fully qualified class names to be loaded
+	 * @param initialize  whether the classes should be initialized upon loading
+	 * @param classLoader the class loader to use for loading the classes
+	 * @return a report containing information about successfully loaded and failed
+	 *         classes
+	 */
 	public static ClassLoadReport loadAllClasses(Collection<String> classNames, boolean initialize, ClassLoader classLoader) {
 		ClassLoadReport report = new ClassLoadReport();
 		for (String className : classNames) {
@@ -37,6 +61,16 @@ public class ClassLoadUtil {
 		return report;
 	}
 	
+	/**
+	 * Loads all classes from the specified ZIP/JAR file using the provided class
+	 * loader.
+	 *
+	 * @param jar         the ZIP/JAR file containing classes
+	 * @param initialize  whether the classes should be initialized upon loading
+	 * @param classLoader the class loader to use for loading the classes
+	 * @return a report containing information about successfully loaded and failed
+	 *         classes
+	 */
 	public static ClassLoadReport loadAllClasses(ZipFile jar, boolean initialize, ClassLoader classLoader) {
 		return loadAllClasses(getAllClassNames(jar), initialize, classLoader);
 	}
